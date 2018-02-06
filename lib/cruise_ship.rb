4c79166a2d3f5452086a0c3c5f652d930a1ee9b2
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-06 23:27:11 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,12 @@ suite_e: "Crumpet the Elf"
  }
 
 def select_winner(passengers)
-  passenger.each do |suites, names|
-    if suites[-1] == 'a' & names[0] = "A"
-      winner.push 
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+ 
+  winner  
 end
